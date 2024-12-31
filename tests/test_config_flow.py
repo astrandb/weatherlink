d3bf0b3f3ed6e6f053f0801947079839def8effd
Åke Strandberg <ake@strandberg.eu>
2024-12-31 14:13:54 +0000

    Improve tests and increase coverage

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -9,10 +9,12 @@ from custom_components.weatherlink.const import (
     DOMAIN,
     CONF_API_KEY_V2,
     CONF_API_SECRET,
+    CONF_API_TOKEN,
     CONF_STATION_ID,
 )
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.data_entry_flow import FlowResultType
 
 
@@ -47,8 +49,8 @@ async def test_succesful_flow(
     assert result["step_id"] == "user_2"
 
     with patch(
-        "custom_components.weatherlink.config_flow.validate_input_v2",
-        return_value={"title": "test"},
+        "custom_components.weatherlink.WLHubV2.authenticate",
+        return_value=True,
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"], {CONF_API_KEY_V2: "123", CONF_API_SECRET: "456"}
@@ -74,10 +76,10 @@ async def test_succesful_flow(
     ],
     ids=["cannot_connect", "invalid_auth", "other_exception"],
 )
-async def test_failed_flow(
+async def test_failed_flow_api_v2_step_2(
     hass: HomeAssistant, bypass_get_all_stations, bypass_get_station, exc, key
 ) -> None:
-    """Test failing flows."""
+    """Test failing flow step 2."""
 
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": config_entries.SOURCE_USER}
@@ -99,6 +101,55 @@ async def test_failed_flow(
     ):
         result = await hass.config_entries.flow.async_configure(
             result["flow_id"], {CONF_API_KEY_V2: "123", CONF_API_SECRET: "456"}
+        )
+
+
+@pytest.mark.parametrize(
+    ("exc", "key"),
+    [
+        (CannotConnect, "cannot_connect"),
+        (InvalidAuth, "invalid_auth"),
+        (Exception, "unknown"),
+    ],
+    ids=["cannot_connect", "invalid_auth", "other_exception"],
+)
+async def test_failed_flow_api_v2_step_3(
+    hass: HomeAssistant, bypass_get_all_stations, bypass_get_station, exc, key
+) -> None:
+    """Test failing flow step 3."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] is None
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"api_version": "api_v2"},
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user_2"
+
+    with patch(
+        "custom_components.weatherlink.config_flow.validate_input_v2", return_value=True
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {CONF_API_KEY_V2: "123", CONF_API_SECRET: "456"}
+        )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user_3"
+
+    with patch(
+        "custom_components.weatherlink.config_flow.validate_input_v2b",
+        side_effect=exc,
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {CONF_STATION_ID: "167531"},
         )
 
     assert result["errors"] == {"base": key}
@@ -133,3 +184,116 @@ async def test_auth_error(
         )
 
     assert result["errors"] == {"base": "invalid_auth"}
+
+
+async def test_succesful_flow_api_v1(
+    hass: HomeAssistant, bypass_get_data_api_1
+) -> None:
+    """Test that we get the form and create the entry using api v1."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] is None
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"api_version": "api_v1"},
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user_1"
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {
+            CONF_USERNAME: "username",
+            CONF_PASSWORD: "password",
+            CONF_API_TOKEN: "token",
+        },
+    )
+
+    assert result["type"] is FlowResultType.CREATE_ENTRY
+
+
+async def test_auth_error_api_v1(
+    hass: HomeAssistant,
+    bypass_get_data_api_1,
+) -> None:
+    """Test auth error."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] is None
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"api_version": "api_v1"},
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user_1"
+
+    with patch(
+        "custom_components.weatherlink.WLHub.authenticate",
+        return_value=False,
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_USERNAME: "username",
+                CONF_PASSWORD: "password",
+                CONF_API_TOKEN: "token",
+            },
+        )
+
+    assert result["errors"] == {"base": "invalid_auth"}
+
+
+@pytest.mark.parametrize(
+    ("exc", "key"),
+    [
+        (CannotConnect, "cannot_connect"),
+        (InvalidAuth, "invalid_auth"),
+        (Exception, "unknown"),
+    ],
+    ids=["cannot_connect", "invalid_auth", "other_exception"],
+)
+async def test_failed_flow_api_1(
+    hass: HomeAssistant, bypass_get_data_api_1, exc, key
+) -> None:
+    """Test failing flows."""
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["errors"] is None
+
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        {"api_version": "api_v1"},
+    )
+
+    assert result["type"] is FlowResultType.FORM
+    assert result["step_id"] == "user_1"
+
+    with patch(
+        "custom_components.weatherlink.config_flow.validate_input", side_effect=exc
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                CONF_USERNAME: "username",
+                CONF_PASSWORD: "password",
+                CONF_API_TOKEN: "token",
+            },
+        )
+
+    assert result["errors"] == {"base": key}
